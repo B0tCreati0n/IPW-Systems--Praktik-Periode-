4b7475bf758b844965d9026d79f79722b859919b
B0t Creati0n <b0tcreati0n@pm.me>
2023-11-08 11:22:16 +0100

    add answare button work again

--- simplePoll/newPoll/newPoll.php
+++ simplePoll/newPoll/newPoll.php
@@ -21,6 +21,7 @@
             <h1>New Poll Creator</h1>
         </div>
         <div>
+            <form method="POST">
                 <p>Please enter your email</p>
                 <input type="email" placeholder="example@example.com" name="B0tAutoherEmail" id="B0tNewPollCreaterEmail" maxlength="255">
                 <p>Poll Name</p>
@@ -30,24 +31,23 @@
                 <br>
                 <input type="text" placeholder="Answer 2" name="Answer2" id="B0tNewPollAnswer2" minlength="5" maxlength="32">
                 <?php 
-                
-
                 function B0tAddNewPollAnswer() {
-                    if ($_SESSION["B0tAnswareCounter"] <= 9) {
+                    if ($_SESSION["B0tAnswareCounter"] < 10) {
                         $_SESSION["B0tAnswareCounter"]++; 
                     }
                 };
-                for ($i=2; $i < ($_SESSION["B0tAnswareCounter"]); $i++){
-                    echo "<br> \n" . '<input type="text" placeholder="Answer ' . $i + 1 . '" name="Answer' . $i + 1 . '" id="B0tNewPollAnswer' . $i + 1 . '" minlength="5" maxlength="32"> ';
+
+                for ($i = 2; $i <= $_SESSION["B0tAnswareCounter"]; $i++) {
+                    echo "<br> \n" . '<input type="text" placeholder="Answer ' . ($i + 1) . '" name="Answer' . ($i + 1) . '" id="B0tNewPollAnswer' . ($i + 1) . '" minlength="5" maxlength="32"> ';
                 }
+
                 if (isset($_POST['B0tNewPollAddAnswerBtn'])) {
                     // Call the PHP function
                     B0tAddNewPollAnswer();
                 }
-
                 ?>
                 <br>
-                <input type="button" value="Add Answer"  name="B0tNewPollAddAnswerBtn" id="B0tNewPollAddAnswerBtn" >
+                <input type="submit" value="Add Answer"  name="B0tNewPollAddAnswerBtn" id="B0tNewPollAddAnswerBtn">
                 <br>
                 <br>
                 <input type="submit" value="Submit" name="B0tNewPollSubmitBtn" id="B0tNewPollSubmitBtn">
