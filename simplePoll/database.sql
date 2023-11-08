c11adcf2afb5a9d08b8bcaef5240b2920d8c89ae
B0t Creati0n <b0tcreati0n@pm.me>
2023-11-08 07:56:10 +0100

    good morning and i have a good cop of coffeecino

--- simplePoll/database.sql
+++ simplePoll/database.sql
@@ -1,11 +1,12 @@
-CREATE TABLE 'pollQustion' (
-    'pollId' bigint NOT NULL,
-    'pollQustion' text NOT NULL,
-    'pollAnswer' bigint NOT NULL,
-    'pollStatus' tinyint NOT NULL,
-    'timeStamp' timeStamp NOT NULL DEFAULT unix_timestamp
+CREATE TABLE `pollQustions` (
+    `pollId` bigint(20) NOT NULL,
+    `pollQustion` text NOT NULL,
+    `pollAnswer` bigint NOT NULL,
+    `pollAnswerVotes` bigint NOT NULL,
+    `pollStatus` tinyint NOT NULL,
+    `timeStamp` timeStamp NOT NULL DEFAULT unix_timestamp
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE 'pollQustion'
-    ADD PRIMARY KEY ('pollId');
-    ADD KEY ('timestamp');
+ALTER TABLE `pollQustions`
+    ADD PRIMARY KEY (`pollId`);
+    ADD KEY (`timestamp`);
--- simplePoll/newPoll/newPoll.php
+++ simplePoll/newPoll/newPoll.php
@@ -21,27 +21,25 @@
             <h1>New Poll Creator</h1>
         </div>
         <div>
-            <form method="post" id="B0tNewPollForm">
                 <p>Please enter your email</p>
-                <input type="email" placeholder="example@example.com" name="Email" id="B0tNewPollCreaterEmail" maxlength="255">
+                <input type="email" placeholder="example@example.com" name="B0tAutoherEmail" id="B0tNewPollCreaterEmail" maxlength="255">
                 <p>Poll Name</p>
-                <input type="text" placeholder="Poll Name" name="Poll Name" id="B0tNewPollName" minlength="5" maxlength="64">
+                <input type="text" placeholder="Poll Name" name="pollName" id="B0tNewPollName" minlength="5" maxlength="64">
                 <p>Answers</p>
-                <input type="text" placeholder="Answer 1" name="Answer 1" id="B0tNewPollAnswer1" minlength="5" maxlength="32">
+                <input type="text" placeholder="Answer 1" name="Answer1" id="B0tNewPollAnswer1" minlength="5" maxlength="32">
                 <br>
-                <input type="text" placeholder="Answer 2" name="Answer 2" id="B0tNewPollAnswer2" minlength="5" maxlength="32">
+                <input type="text" placeholder="Answer 2" name="Answer2" id="B0tNewPollAnswer2" minlength="5" maxlength="32">
                 <?php 
                 
 
                 function B0tAddNewPollAnswer() {
                     if ($_SESSION["B0tAnswareCounter"] <= 9) {
-                        $_SESSION["B0tAnswareCounter"]++;
-                        echo "<br> \n" . '<input type="text" placeholder="Answer ' . $_SESSION["B0tAnswareCounter"] . '" name="Answer' . $_SESSION["B0tAnswareCounter"] . '" id="B0tNewPollAnswer' . $_SESSION["B0tAnswareCounter"] . '" minlength="5" maxlength="32"> ';
-                    } else {
-                        syslog(LOG_INFO, "Max amount of answers");
+                        $_SESSION["B0tAnswareCounter"]++; 
                     }
                 };
-
+                for ($i=2; $i < ($_SESSION["B0tAnswareCounter"]); $i++){
+                    echo "<br> \n" . '<input type="text" placeholder="Answer ' . $i + 1 . '" name="Answer' . $i + 1 . '" id="B0tNewPollAnswer' . $i + 1 . '" minlength="5" maxlength="32"> ';
+                }
                 if (isset($_POST['B0tNewPollAddAnswerBtn'])) {
                     // Call the PHP function
                     B0tAddNewPollAnswer();
@@ -49,7 +47,7 @@
 
                 ?>
                 <br>
-                <input type="submit" value="Add Answer"  name="B0tNewPollAddAnswerBtn" id="B0tNewPollAddAnswerBtn" >
+                <input type="button" value="Add Answer"  name="B0tNewPollAddAnswerBtn" id="B0tNewPollAddAnswerBtn" >
                 <br>
                 <br>
                 <input type="submit" value="Submit" name="B0tNewPollSubmitBtn" id="B0tNewPollSubmitBtn">
@@ -58,3 +56,27 @@
     </div>
 </body>
 </html>
+
+<!--Collect's information from FORM's-->
+<?php 
+function B0tSaveFormData() {
+    $B0tGetAnsware = array();
+
+    for ($i = 1; $i <= 9; $i++) {
+        $answerKey = "Answer" . $i;
+        if (isset($_POST[$answerKey])) {
+            $B0tGetAnsware[] = $_POST[$answerKey];
+        }
+    }
+    $B0tGetPollName = $_POST["pollName"];
+    $B0tGetPollEmail = $_POST["email"];
+
+};
+
+if (isset($_POST['B0tNewPollSubmitBtn'])) {
+    // Call the PHP function
+    B0tSaveFormData();
+};
+
+
+?>
