a5d18810e9e42fee9904342d3d01419d890fe891
B0t Creati0n <b0tcreati0n@pm.me>
2023-11-09 15:12:18 +0100

    back from feature review

--- simplePoll/index.php
+++ simplePoll/index.php
@@ -1,3 +1,49 @@
+<?php
+// Moved the function definition to the top for better organization
+function generateRandomString($length = 32) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $randomString = '';
+
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, strlen($characters) - 1)];
+    }
+
+    return $randomString;
+}
+
+session_start();
+
+// Session token to keep
+$sessionToKeep = 'B0tUserIdToken';
+
+if (!isset($_SESSION[$sessionToKeep])) {
+    $_SESSION[$sessionToKeep] = generateRandomString();
+}
+
+// Set the lifespan for the specified session token
+$_SESSION[$sessionToKeep.'_expires'] = time() + 157680000;
+
+// Loop through all session variables
+foreach ($_SESSION as $key => $value) {
+    // Check if the variable name is not the one you want to keep
+    if ($key !== $sessionToKeep.'_expires') {
+        // Set the default lifespan to 900 seconds for other session variables
+        $_SESSION[$key.'_expires'] = time() + 900;
+        
+        // Unset the session variable if it exceeds the default lifespan
+        if ($_SESSION[$key.'_expires'] < time()) {
+            unset($_SESSION[$key]);
+        }   
+    }
+}
+
+// Destroy the session if the specified token exceeds its lifespan
+if ($_SESSION[$sessionToKeep.'_expires'] < time()) {
+    session_destroy();
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
